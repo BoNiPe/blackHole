d207f1d68ed1d0de90a35bdb547458e8443fd900
Desting <Nik_zd@hotmail.com>
2014-09-09 10:53:17 +0200

    Initial Commit

---
+++ src/utilities/Utilities.java
@@ -0,0 +1,20 @@
+package utilities;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+public class Utilities {
+
+    public static Properties initProperties(String propertyFile) {
+        Properties properties = new Properties();
+        try (InputStream is = new FileInputStream(propertyFile)) {
+            properties.load(is);
+        } catch (IOException ex) {
+            System.out.println(String.format("Could not locate the %1$s file.", propertyFile));
+            return null;
+        }
+        return properties;
+    }
+}
